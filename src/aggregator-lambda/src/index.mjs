ecba54e1bb75091b011906cee5d4d04c9c6f7724
Kurtis Mash <24705116+kurtismash@users.noreply.github.com>
2024-06-02 17:27:40 +0100

    Implement memory caching with aggregator-function

--- src/aggregator-lambda/src/index.mjs
+++ src/aggregator-lambda/src/index.mjs
@@ -5,27 +5,69 @@ import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
 
 const DDB_LOCK_TABLE = process.env.DDB_LOCK_TABLE;
 const OUTPUT_SQS_URL = process.env.OUTPUT_SQS_URL;
+const MAX_SECONDARY_BATCHING_WINDOW = process.env.MAX_SECONDARY_BATCHING_WINDOW || 60;
+const MAX_BATCH_SIZE = process.env.MAX_BATCH_SIZE || 100;
+
+const BATCH_CACHE = {};
 
 const sqsClient = new SQSClient();
 const dbClient = new DynamoDBClient();
 const docClient = DynamoDBDocumentClient.from(dbClient);
 
-export const handler = async (event, context) => {
-	const rawMessageCount = event.Records.length;
-	let filteredMessageCount = 0;
+const startNewBatch = async (sourceId, lambdaTimeoutTime) => {
+	console.log(BATCH_CACHE);
 	const batchId = v4();
-
-	// Send a message to the batch-queue with a batch identifier.
-	// Do this first to handle the batch even if this Lambda fails.
+	const expires = lambdaTimeoutTime + MAX_SECONDARY_BATCHING_WINDOW * 1000;
+	BATCH_CACHE[sourceId] = {
+		batchId,
+		expires,
+		items: 1,
+	};
 	const sqsCommand = new SendMessageCommand({
 		QueueUrl: OUTPUT_SQS_URL,
-		DelaySeconds: context.getRemainingTimeInMillis() / 1000,
+		DelaySeconds: Math.ceil((expires - Date.now()) / 1000),
 		MessageBody: JSON.stringify({ batchId }),
 	});
 	await sqsClient.send(sqsCommand);
+	return batchId;
+};
+
+const getBatchId = async (sourceId, lambdaTimeoutTime) => {
+	const currentBatchForSource = BATCH_CACHE[sourceId];
+
+	if (!currentBatchForSource) {
+		console.log({
+			action: "Start new batch.",
+			reason: "No existing batch.",
+			source: sourceId,
+		});
+		return startNewBatch(sourceId, lambdaTimeoutTime);
+	} else if (lambdaTimeoutTime >= currentBatchForSource.expires) {
+		console.log({
+			action: "Start new batch.",
+			reason: "Expiry before lambda timeout.",
+			source: sourceId,
+		});
+		return startNewBatch(sourceId, lambdaTimeoutTime);
+	} else if (currentBatchForSource.items >= MAX_BATCH_SIZE) {
+		console.log({
+			action: "Start new batch.",
+			reason: "Max batch size exceeded.",
+			source: sourceId,
+		});
+		return startNewBatch(sourceId, lambdaTimeoutTime);
+	} else {
+		currentBatchForSource.items += 1;
+		return currentBatchForSource.batchId;
+	}
+};
+
+export const handler = async (event, context) => {
+	const lambdaTimeoutTime = Date.now() + context.getRemainingTimeInMillis();
 
 	// Attempt to add each message to DynamoDB.
 	const promises = event.Records.map(async (i) => {
+		const batchId = await getBatchId(i.eventSourceARN, lambdaTimeoutTime);
 		const { identifier } = JSON.parse(i.body);
 		const ddbCommand = new PutCommand({
 			TableName: DDB_LOCK_TABLE,
@@ -38,7 +80,6 @@ export const handler = async (event, context) => {
 		});
 		try {
 			await docClient.send(ddbCommand);
-			filteredMessageCount += 1;
 		} catch (e) {
 			return { success: false, sqsMessageId: i.messageId };
 		}
@@ -48,7 +89,7 @@ export const handler = async (event, context) => {
 	const results = await Promise.all(promises);
 	const successCount = results.filter((i) => i.success).length;
 	const failures = results.filter((i) => !i.success);
-	console.log(`${rawMessageCount} -> ${successCount} (Batch: ${batchId})`);
+	console.log(`Success: ${successCount}. Failures: ${failures.length}`);
 	return {
 		batchItemFailures: failures.map((i) => ({
 			itemIdentifier: i.sqsMessageId,
