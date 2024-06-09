4a74a954dd263fa10ab5cd66c8d911a930c71365
Kurtis Mash <24705116+kurtismash@users.noreply.github.com>
2024-06-09 15:12:56 +0100

    Implement retries for `UnprocessedItems`

--- src/batch-processor-lambda/src/index.mjs
+++ src/batch-processor-lambda/src/index.mjs
@@ -1,7 +1,4 @@
-import {
-	BatchWriteItemCommand,
-	DynamoDBClient,
-} from "@aws-sdk/client-dynamodb";
+import { BatchWriteItemCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { QueryCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const DDB_LOCK_TABLE = process.env.DDB_LOCK_TABLE;
@@ -10,15 +7,15 @@ const DDB_BATCH_INDEX = process.env.DDB_BATCH_INDEX;
 const dbClient = new DynamoDBClient();
 const docClient = DynamoDBDocumentClient.from(dbClient);
 
-async function deleteItems(ids) {
-	const MAX_BATCH_SIZE = 25;
-	const batches = [];
-	for (let i = 0; i < ids.length; i += MAX_BATCH_SIZE) {
-		batches.push(ids.slice(i, i + MAX_BATCH_SIZE));
+const deleteBatchWithRetries = async (batch, retries = 0) => {
+	const MAX_RETRIES = 5;
+	const RETRY_DELAY_MS = 200;
+	if (retries > MAX_RETRIES) {
+		throw new Error(`Failed to delete batch after ${MAX_RETRIES} retries`);
 	}
-	const promises = batches.map((batch) => {
-		console.log("Sending delete request for messages:", batch);
-		return dbClient.send(
+
+	try {
+		const response = await dbClient.send(
 			new BatchWriteItemCommand({
 				RequestItems: {
 					[DDB_LOCK_TABLE]: batch.map((id) => ({
@@ -31,9 +28,43 @@ async function deleteItems(ids) {
 				},
 			})
 		);
-	});
+
+		const unprocessedItems = response.UnprocessedItems || {};
+
+		// Check if there are unprocessed items
+		if (unprocessedItems[DDB_LOCK_TABLE] && unprocessedItems[DDB_LOCK_TABLE].length > 0) {
+			console.log(
+				`Unprocessed items found: ${unprocessedItems[DDB_LOCK_TABLE].map((item) => item.DeleteRequest.Key.messageId.S)}`
+			);
+
+			// Retry only unprocessed items
+			await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY_MS));
+			await processBatchWithRetries(
+				unprocessedItems[DDB_LOCK_TABLE].map((item) => item.DeleteRequest.Key.messageId.S),
+				retries + 1
+			);
+		}
+	} catch (error) {
+		console.error(`Error deleting batch: ${error}`);
+		// Retry in case of an error if the retry count is less than MAX_RETRIES
+		if (retries < MAX_RETRIES) {
+			await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY_MS));
+			await deleteBatchWithRetries(batch, retries + 1);
+		} else {
+			throw error;
+		}
+	}
+};
+
+const deleteItems = async (ids) => {
+	const MAX_BATCH_SIZE = 25;
+	const batches = [];
+	for (let i = 0; i < ids.length; i += MAX_BATCH_SIZE) {
+		batches.push(ids.slice(i, i + MAX_BATCH_SIZE));
+	}
+	const promises = batches.map((i) => processBatchWithRetries(i));
 	await Promise.all(promises);
-}
+};
 
 async function query(batchId) {
 	const params = {
